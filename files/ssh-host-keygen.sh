b01ca0e09618cffa07ab285dcf51660b438ba23d
Christian Wagner <chriswayg@gmail.com>
2020-04-26 02:35:45 +0800

    Initial commit for Alpine

---
+++ files/ssh-host-keygen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Ansible managed
+
+# OpenSSH Server Key Re-Generation (on first boot in new VM)
+if [[ -f /etc/run-ssh-host-keygen-once ]]; then
+    # remove all host keys first, so that all will be refreshed
+    rm -v -f /etc/ssh/*key*
+    ssh-keygen -A
+    rm -v -f /etc/run-ssh-host-keygen-once
+fi
