f8691e7a407070d1ac5f20513530552a5d2b5bb7
Christian Wagner <chriswayg@gmail.com>
2020-05-05 00:24:38 +0800

    added OpneBSD script for OpenSSH Server Key Re-Generation on first boot in new VM

--- files/ssh-host-keygen.sh
+++ files/ssh-host-keygen.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 # Ansible managed
-# Alpine
+# OpenBSD
 
-# OpenSSH Server Key Re-Generation - Alpine (on first boot in new VM)
-if [[ -f /etc/run-ssh-host-keygen-once ]]; then
+# OpenSSH Server Key Re-Generation on first boot in new VM
+if [ -f /etc/run-ssh-host-keygen-once ]; then
     # remove all host keys first, so that all will be refreshed
     rm -v -f /etc/ssh/*key*
     ssh-keygen -A
-    # ensure that /etc/issue gets updated with the new key
-    /etc/network/if-up.d/show-ip-address update
     rm -v -f /etc/run-ssh-host-keygen-once
 fi
