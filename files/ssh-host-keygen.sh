34f5f787a12b57b947d7428c7c517f742e2c31f1
Christian Wagner <chriswayg@gmail.com>
2020-05-05 03:46:20 +0800

    reduce output from script

--- files/ssh-host-keygen.sh
+++ files/ssh-host-keygen.sh
@@ -5,7 +5,7 @@
 # OpenSSH Server Key Re-Generation on first boot in new VM
 if [ -f /etc/run-ssh-host-keygen-once ]; then
     # remove all host keys first, so that all will be refreshed
-    rm -v -f /etc/ssh/*key*
+    rm -f /etc/ssh/*key*
     ssh-keygen -A
-    rm -v -f /etc/run-ssh-host-keygen-once
+    rm -f /etc/run-ssh-host-keygen-once
 fi
